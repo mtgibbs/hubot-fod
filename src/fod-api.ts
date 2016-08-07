ba6a60b53b2f0009b0e6a0be48e5a16844cc7e5c
Matt Gibbs <matt@mtgibbs.xyz>
2016-08-07 14:02:51 -0400

    Adding a helper class for API paths

--- src/fod-api.ts
+++ src/fod-api.ts
@@ -2,6 +2,26 @@
 
 import * as request from 'request';
 
+export class FoDApiHelper {
+
+    private static _DEFAULT_URI: string = 'hpfod.com';
+    private static _DEFAULT_PROTOCOL: string = 'https';
+
+    public static getApiUri(path?: string): string {
+        let uri = `${this._DEFAULT_PROTOCOL}://api.${process.env.HUBOT_FOD_BASEURI || this._DEFAULT_URI}`;
+        if (path)
+            uri += path;
+        return uri;
+    }
+
+    public static getSiteUri(path?: string): string {
+        let uri = `${this._DEFAULT_PROTOCOL}://${process.env.HUBOT_FOD_BASEURI || this._DEFAULT_URI}`;
+        if (path)
+            uri += path;
+        return uri;
+    }
+}
+
 export class FoDApi {
 
     private _protocol: string = 'https'; // this could be configurable later if the bot developer was testing locally
